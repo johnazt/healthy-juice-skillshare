ef8b93b0d6cc94a677356f72a268ec31a337600e
John Asto <john.azt.dev@gmail.com>
2023-10-17 23:31:22 -0300

    delete actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	build: {
-		outDir: 'docs',
-		base: './'
-	}
+	base: 'https://johnazt.github.io/healthy-juice-skillshare/'
 });
